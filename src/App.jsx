d05088f6cf0d4925a45c827f4302f6cf6e289c28
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-02-04 11:56:22 +0530

    Added Intro Animation

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,16 @@ import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 // import ParticleBaground from "./components/ParticleBaground";
 import CustomCoursor from "./components/CustomCoursor"
+import React from "react";
+import IntroAnimation from "./components/IntroAnimation";
 
 const App = () => {
+  const [introDone , setIntroDone] = React.useState(false);
   return (
+    <>
+    {!introDone && <IntroAnimation onFinish={()=> setIntroDone(true)}/>}
+
+      {introDone && (
     <div className=" relative gradiant text-white">
       <CustomCoursor/>
       {/* <ParticleBaground /> */}
@@ -26,6 +33,8 @@ const App = () => {
       <Footer />
 
     </div>
+    )}
+    </>
   )
 }
 
